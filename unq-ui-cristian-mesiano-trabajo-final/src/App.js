ba54f4fa66236ade95d7d365963fbd7eee71b603
KBRIEL <32989204+KBRIEL@users.noreply.github.com>
2021-07-11 18:06:17 -0300

    Add files via upload
    primer parte tp integrador

---
+++ unq-ui-cristian-mesiano-trabajo-final/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Generala from './components/Generala';
+import Dado from './components/Dado';
+import Tablero from './components/Tablero';
+
+
+
+function App() {
+  return (
+    <div className="App-link ">
+      <h1  className="letras-dados centrado">GENERALA</h1>
+     <Generala/>
+  
+    </div>
+  );
+}
+
+export default App;
---
+++ unq-ui-cristian-mesiano-trabajo-final/src/components/Generala.js
@@ -0,0 +1,149 @@
+import { useEffect, useState } from "react";
+import dado from './dado.png';
+import d from './d.png';
+import '../App.css';
+import Dado from "./Dado";
+import Tablero from "./Tablero";
+
+
+ function Generala() {
+
+return(
+<div>
+{/* <Tablero/> */}
+  <Dado/>
+  
+</div>
+
+);
+
+// var [puntos1,setPuntos1]= useState(0);
+// var [puntos2,setPuntos2]= useState(0);
+// var [puntos3,setPuntos3]= useState(0);
+// var [puntos4,setPuntos4]= useState(0);
+// var [puntos5,setPuntos5]= useState(0);
+
+// const [tirar1,setTirar1]=useState(true);
+// const [tirar2,setTirar2]=useState(true);
+// const [tirar3,setTirar3]=useState(true);
+// const [tirar4,setTirar4]=useState(true);
+// const [tirar5,setTirar5]=useState(true);
+
+
+// function getRandom(min, max) {
+//     return Math.floor(Math.random() * (max - min)) + min;
+//   }
+
+
+// const lanzar=()=>{
+//   dado1();
+//   dado2();
+//   dado3();
+//   dado4();
+//   dado5();
+   
+// }
+// const dado1=()=>{
+//   if(tirar1){
+//     setPuntos1(getRandom(1,6))
+//    }
+  
+// }
+
+// const dado2=()=>{
+//   if(tirar2){
+//     setPuntos2(getRandom(1,6))
+//    }
+  
+// }
+
+// const dado3=()=>{
+//   if(tirar3){
+//     setPuntos3(getRandom(1,6))
+
+//    }
+  
+// }
+
+// const dado4=()=>{
+//   if(tirar4){
+//     setPuntos4(getRandom(1,6))
+
+//    }
+  
+// }
+
+// const dado5=()=>{
+//   if(tirar5){
+//     setPuntos5(getRandom(1,6))
+
+//   }
+  
+// }
+
+// const elegir1=()=>{
+//   setTirar1(!tirar1);
+// }
+// const elegir2=()=>{
+//   setTirar2(!tirar2);
+// }
+// const elegir3=()=>{
+//   setTirar3(!tirar3);
+// }
+// const elegir4=()=>{
+//   setTirar4(!tirar4);
+// }
+// const elegir5=()=>{
+//   setTirar5(!tirar5);
+// }
+
+
+
+
+// useEffect(()=>{
+
+
+
+// },[])
+
+
+//     return (
+//   <div className="center-k">
+//       <div >
+//         <div>
+          
+//           <h1 className="letras-dados">DADO1 : {puntos1}</h1>
+//           <img src={d} width="3%"/>
+//           <button className="btn-select" onClick={elegir1}>{!tirar1 ? <h3>SELECTED</h3>:<h3>TO SELECT</h3>}</button>
+//         </div>
+//         <div>
+//           <h1 className="letras-dados">DADO2 : {puntos2}</h1>
+//           <img src={d} width="3%"/>
+//           <button className="btn-select" onClick={elegir2}>{!tirar2 ? <h3>SELECTED</h3>:<h3>TO SELECT</h3>}</button>
+//         </div>  
+//         <div>
+//           <h1 className="letras-dados">DADO3 : {puntos3}</h1>
+//           <img src={d} width="3%"/>
+//           <button className="btn-select" onClick={elegir3}>{!tirar3 ? <h3>SELECTED</h3>:<h3>TO SELECT</h3>}</button>
+//         </div>
+//         <div>
+//           <h1 className="letras-dados">DADO4 : {puntos4}</h1>
+//           <img src={d} width="3%"/>
+//           <button className="btn-select" onClick={elegir4}>{!tirar4 ? <h3>SELECTED</h3>:<h3>TO SELECT</h3>}</button>
+//         </div>
+//         <div>
+//           <h1 className="letras-dados">DADO5 : {puntos5}</h1>
+//           <img src={d} width="3%"/>
+//           <button className="btn-select" onClick={elegir5}>{!tirar5 ? <h3>SELECTED</h3>:<h3>TO SELECT</h3>}</button>
+//         </div>
+    
+//       </div>
+//           <button onClick={lanzar}><h1>THROW</h1></button>
+//   </div>
+//     );
+
+
+
+  }
+  
+  export default Generala;
